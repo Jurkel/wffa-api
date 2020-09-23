cce369937c1f4a01e1ae8a0fdbd77eec5e7fe0fa
Harvey Le <harveyxcle@gmail.com>
2020-09-23 12:30:49 -0400

    Updated knex to raw statements

--- src/player/player-service.js
+++ src/player/player-service.js
@@ -3,28 +3,28 @@ const PlayerService = {
         return knex.select('player_info').from('player');
     },
     getPlayerById(knex, id) {
-        return knex.select('player_info-> ' + id).from('player');
+        return knex.raw("select player_info-> '" + id + "'").from('player');
     },
     getPlayersByIds(knex, ids) {
-        let selectStatements = '';
+        let selectStatements = [];
         let playerIds = ids.split(',');
         let totalPlayers = playerIds.length;
 
-        retrieveSelectStatements = (statements) => {
+        unionAllStatements = (statements) => {
             return statements;
         }
         
         let completeSelectStatements = playerIds.forEach((id, index) => {
             let select = getPlayerById(knex, id);
-            if(index == totalPlayers - 1) {
-                selectStatements += select;
-                retrieveSelectStatements(selectStatements);
-            } else {
-                selectStatements += select + 'UNION ALL'
-            }
+            if(index > 0) {
+                selectStatements.push(select);
+            }else if(index == totalPlayers - 1) {
+                selectStatements.push(select);
+                unionAll(selectStatements);
+            } 
         })
 
-        return completeSelectStatements;
+        return knex.raw("select player_info-> '" + completeSelectStatements[0] + "'").from('player').unionAll(selectStatements);
     },
     updatePlayerInfo(knex, data) {
         let dataString = JSON.stringify(data).replace(/'/g, '');
